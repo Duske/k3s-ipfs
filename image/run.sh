534fa835eb2e3efdb67734e003dd6b5d81b00eb0
Duske <mail@duske.me>
2019-04-21 16:30:05 +0200

    [task] add config to insert custom bootstrap node

--- image/run.sh
+++ image/run.sh
@@ -6,6 +6,10 @@ set -x
 
 ipfs init
 ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/9001
+if [ -z "$IPFS_BOOTSTRAP" ]; 
+then echo "Use default bootstrap"; 
+else ipfs bootstrap rm --all && ipfs bootstrap add $IPFS_BOOTSTRAP && echo "Use custom bootstrap peer"; 
+fi
 # Run ipfs with Fuse mount background
 ipfs daemon --mount &
 
